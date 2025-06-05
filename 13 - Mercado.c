d65ea695f01780748fa102382d6f645785ea3ec7
Lis <elisinha220419@gmail.com>
2025-06-05 13:59:03 -0300

    Add files via upload

---
+++ 13 - Mercado.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+typedef struct{
+	char nome[20];
+	int qntd;
+	float preco;
+} Mercado;
+
+void valort(Mercado produtos[], int tam);
+
+int main(){
+	Mercado produtos[10];
+	
+	printf("Bem vindo ao Mercado - SUPER COMPRA - \n");
+	for (int i=0;i<10;i++){
+		printf("Insira o nome do produto %d: ",i+ 1);
+		scanf(" %[^\n]", produtos[i].nome);
+		printf("Insira a quantidade do produto %d: ",i+ 1);
+		scanf("%d", &produtos[i].qntd);
+		printf("Insira o preço do produto %d: ",i+ 1);
+		scanf("%f", &produtos[i].preco);
+	}
+	valort (produtos, 10);
+	
+	return 0;
+}
+
+void valort(Mercado produtos[], int tam) {
+    float valort=0;
+    for (int i=0;i<tam;i++) {
+        valort+=produtos[i].qntd * produtos[i].preco;
+    }
+    printf("O valor total de todos os produtos em estoque é de R$ %.2f\n", valort);
+}
